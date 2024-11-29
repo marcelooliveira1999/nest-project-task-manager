92510132c6e90999669fa4a1e9d981d7830f5467
marcelo.oliveira <marcelo.oliveira@gftcredmais.com.br>
2024-11-29 12:12:20 -0300

    feat: Database
    - TypeORM Config Service implemented
    - TypeORM Module imported into APP Module
    
    ...

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,15 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { TypeOrmConfigService } from './config/typeorm.config.service';
 
 @Module({
   imports: [
     ConfigModule.forRoot({
       isGlobal: true
+    }),
+    TypeOrmModule.forRootAsync({
+      useClass: TypeOrmConfigService
     })
   ]
 })
