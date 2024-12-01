f8d6e558689cfef9752100f79bd2786179c98eda
marcelooliveira1999 <f.marcelo.qxda@gmail.com>
2024-12-01 19:08:57 -0300

    feat: database migrations
    - create user table migration generated
    
    ...

---
+++ src/db/migrations/1733059666666-create_user_table.ts
@@ -0,0 +1,14 @@
+import { MigrationInterface, QueryRunner } from "typeorm";
+
+export class CreateUserTable1733059666666 implements MigrationInterface {
+    name = 'CreateUserTable1733059666666'
+
+    public async up(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.query(`CREATE TABLE "user" ("id" uuid NOT NULL DEFAULT uuid_generate_v4(), "name" character varying NOT NULL, "email" character varying NOT NULL, "password" character varying NOT NULL, "created_at" TIMESTAMP NOT NULL DEFAULT now(), "updated_at" TIMESTAMP NOT NULL DEFAULT now(), "deleted_at" TIMESTAMP, CONSTRAINT "PK_cace4a159ff9f2512dd42373760" PRIMARY KEY ("id"))`);
+    }
+
+    public async down(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.query(`DROP TABLE "user"`);
+    }
+
+}
