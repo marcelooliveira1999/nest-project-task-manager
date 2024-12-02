ce77da8e1bb683ca449cb613f76e34e0b942bd78
marcelo.oliveira <marcelo.oliveira@gftcredmais.com.br>
2024-12-02 15:01:12 -0300

    feat: Root User Seeder add do AppModule providers and runner on main.ts

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,12 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
+import { JwtModule } from '@nestjs/jwt';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { AuthModule } from './auth/auth.module';
+import { JwtConfigService } from './config/jwt.config.service';
 import { TypeOrmConfigService } from './config/typeorm.config.service';
+import { RootUserSeeder } from './db/seeders/root-user.seeder';
+import { PasswordHashPipe } from './helpers/custom-pipes/password-hash.pipe';
 import { TaskModule } from './task/task.module';
 import { UserModule } from './user/user.module';
 
@@ -13,8 +18,14 @@ import { UserModule } from './user/user.module';
     TypeOrmModule.forRootAsync({
       useClass: TypeOrmConfigService
     }),
+    JwtModule.registerAsync({
+      useClass: JwtConfigService,
+      global: true
+    }),
     TaskModule,
-    UserModule
-  ]
+    UserModule,
+    AuthModule
+  ],
+  providers: [RootUserSeeder, PasswordHashPipe]
 })
 export class AppModule {}
