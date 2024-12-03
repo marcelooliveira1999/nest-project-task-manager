8c7043b84241fd8bb0da8a2b3adec046d138bbfa
marcelooliveira1999 <f.marcelo.qxda@gmail.com>
2024-12-02 22:48:40 -0300

    doc: Swagger Setup
    - Create task response dto and message response dto
    - Response of task method mapped to documentation
    - Swagger server started
    
    ...

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,7 @@
-import { ValidationPipe } from '@nestjs/common';
+import { ClassSerializerInterceptor, ValidationPipe } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
-import { NestFactory } from '@nestjs/core';
+import { NestFactory, Reflector } from '@nestjs/core';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 import { RootUserSeeder } from './db/seeders/root-user.seeder';
 
@@ -15,8 +16,19 @@ import { RootUserSeeder } from './db/seeders/root-user.seeder';
     })
   );
 
+  app.useGlobalInterceptors(new ClassSerializerInterceptor(app.get(Reflector)));
   const rootUserSeeder = app.get<RootUserSeeder>(RootUserSeeder);
   await rootUserSeeder.run();
+
+  const config = new DocumentBuilder()
+    .setTitle('Task Manager')
+    .setDescription('API para gerenciamento de tarefas')
+    .setVersion('1.0')
+    .addBearerAuth()
+    .build();
+
+  const documentFactory = () => SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api/docs', app, documentFactory);
 
   const configService = app.get<ConfigService>(ConfigService);
   const port: number = configService.get<number>('PORT');
--- src/task/task.controller.ts
+++ src/task/task.controller.ts
@@ -8,58 +8,121 @@ import {
   Post,
   UseGuards
 } from '@nestjs/common';
+import {
+  ApiBearerAuth,
+  ApiBody,
+  ApiParam,
+  ApiResponse,
+  PartialType
+} from '@nestjs/swagger';
 import { AuthGuard } from 'src/auth/auth.guard';
 import { CreateTaskDto } from './dto/create-task.dto';
+import { MessageResponseDto } from './dto/message-response.dto';
+import { TaskResponseDto } from './dto/task-response.dto';
 import { UpdateTaskDto } from './dto/update-task.dto';
 import { TaskStatusEnum } from './enum/task-status.enum';
 import { TaskService } from './task.service';
 
 @Controller('task')
 @UseGuards(AuthGuard)
+@ApiBearerAuth()
 export class TaskController {
   constructor(private readonly taskService: TaskService) {}
 
   @Post()
+  @ApiResponse({
+    status: 201,
+    description: 'Task created',
+    type: TaskResponseDto
+  })
   create(@Body() createTaskDto: CreateTaskDto) {
     return this.taskService.create(createTaskDto);
   }
 
   @Get()
+  @ApiResponse({
+    status: 200,
+    description: 'Tasks found',
+    type: [TaskResponseDto]
+  })
   findAll() {
     return this.taskService.findAll();
   }
 
   @Get(':id')
+  @ApiParam({ name: 'id', description: 'Task ID' })
+  @ApiResponse({
+    status: 200,
+    description: 'Task found',
+    type: TaskResponseDto
+  })
   findOne(@Param('id') id: string) {
     return this.taskService.findOne(+id);
   }
 
   @Patch(':id')
+  @ApiParam({ name: 'id', description: 'Task ID' })
+  @ApiBody({ type: PartialType(CreateTaskDto) })
+  @ApiResponse({
+    status: 200,
+    description: 'Task updated',
+    type: TaskResponseDto
+  })
   update(@Param('id') id: string, @Body() updateTaskDto: UpdateTaskDto) {
     return this.taskService.update(+id, updateTaskDto);
   }
 
   @Delete(':id')
+  @ApiParam({ name: 'id', description: 'Task ID' })
+  @ApiResponse({
+    status: 200,
+    description: 'Task deleted',
+    type: MessageResponseDto
+  })
   remove(@Param('id') id: string) {
     return this.taskService.remove(+id);
   }
 
   @Patch(':id/pending')
+  @ApiParam({ name: 'id', description: 'Task ID' })
+  @ApiResponse({
+    status: 200,
+    description: 'Task status updated',
+    type: MessageResponseDto
+  })
   toPending(@Param('id') id: string) {
     return this.taskService.updateStatus(+id, TaskStatusEnum.PENDING);
   }
 
   @Patch(':id/in_progress')
+  @ApiParam({ name: 'id', description: 'Task ID' })
+  @ApiResponse({
+    status: 200,
+    description: 'Task status updated',
+    type: MessageResponseDto
+  })
   toProgress(@Param('id') id: string) {
     return this.taskService.updateStatus(+id, TaskStatusEnum.IN_PROGRESS);
   }
 
   @Patch(':id/completed')
+  @ApiParam({ name: 'id', description: 'Task ID' })
+  @ApiResponse({
+    status: 200,
+    description: 'Task status updated',
+    type: MessageResponseDto
+  })
   toCompleted(@Param('id') id: string) {
     return this.taskService.updateStatus(+id, TaskStatusEnum.COMPLETED);
   }
 
   @Patch(':id/cancelled')
+  @ApiParam({ name: 'id', description: 'Task ID' })
+  @ApiResponse({
+    status: 200,
+    description: 'Task status updated',
+    type: MessageResponseDto
+  })
   toCancelled(@Param('id') id: string) {
     return this.taskService.updateStatus(+id, TaskStatusEnum.CANCELLED);
   }
